ed67eed3939b876a241512f6ef2c50f3c1bb3272
her0807 <her0807@naver.com>
2021-09-01 18:08:26 +0900

    dfs 로 변환

--- Algorithm/src/com/sudal/B11725.java
+++ Algorithm/src/com/sudal/B11725.java
@@ -5,8 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class B11725 {
@@ -22,11 +20,12 @@ public class B11725 {
         FastReader scan = new FastReader();
         N = scan.nextInt();
 
-
+        adj = new ArrayList[N + 1];
         parent = new int[N + 1];
-        for (int i = 1; i <= N; i++)
-            adj[i] = new ArrayList<>();
-        for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            adj[j] = new ArrayList<>();
+        }
+        for (int i = 1; i < N; i++) {
             int x = scan.nextInt();
             int y = scan.nextInt();
             adj[x].add(y);
@@ -34,32 +33,17 @@ public class B11725 {
         }
     }
 
-    // start 에서 시작해서 갈 수 있는 정점들을 모두 탐색하기
-    static void bfs(int start) {
-        Queue<Integer> que = new LinkedList<>();
-
-        // start는 방문 가능한 점이므로 que에 넣어준다.
-        que.add(start);
-        visit[start] = true;  // start를 갈 수 있다고 표시하기 (중요!!!)
-
-        while (!que.isEmpty()) {  // 더 확인할 점이 없다면 정지
-            int x = que.poll();
-
-            for (int y: adj[x]){
-                if (visit[y]) continue;  // x 에서 y 를 갈 수는 있지만, 이미 탐색한 점이면 무시
-
-                // y를 갈 수 있으니까 que에 추가하고, visit 처리 하기!
-                que.add(y);
-                parent[y] = x;
-                visit[y] = true;
-            }
+    static void dfs(int x, int par) {
+        for (int y : adj[x]) {
+            if (y == par) continue;
+            parent[y] = x;
+            dfs(y, x);
         }
     }
 
     static void pro() {
         visit = new boolean[N + 1];
-        bfs(1);
-        int ans = 0;
+        dfs(1, -1);
         for (int i = 2; i <= N; i++)
             sb.append(parent[i]).append('\n');
         System.out.println(sb);
