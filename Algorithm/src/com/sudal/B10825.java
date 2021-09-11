a13e990dd22ef1d399ac57490b28228786daf2a2
her0807 <her0807@naver.com>
2021-09-11 12:02:09 +0900

    feat : sort

---
+++ Algorithm/src/com/sudal/B10825.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+public class B10825 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static class Elem implements Comparable<Elem> {
+
+        public String name;
+        public int korean, english, math;
+
+        @Override
+        public int compareTo(Elem other) {
+            if (korean != other.korean) return other.korean - korean; // 국어 내림차순
+            if (english != other.english) return english - other.english;// 영어 내림차순
+            if (math != other.math) return other.math - math; // 수학 내림차순
+            return name.compareTo(other.name); // 그래도 같으면 이름을 정
+        }
+    }
+
+    static int N;
+    static Elem[] a;
+
+    static void input() {
+        N = scan.nextInt();
+        a = new Elem[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = new Elem();
+            a[i].name = scan.next();
+            a[i].korean = scan.nextInt();
+            a[i].english = scan.nextInt();
+            a[i].math = scan.nextInt();
+        }
+    }
+
+    static void pro() {
+        // TODO
+       Arrays.sort(a);
+
+       for(int i = 0; i<N;i++)
+          sb.append(a[i].name).append("\n");
+
+        System.out.println(sb.toString());
+        // 정답 출력하기
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
