c62ea58e2d5f555f392540202bc753123dbc977f
her0807 <her0807@naver.com>
2021-09-21 20:49:43 +0900

    docs : 스프링에 대한 이해를 위한 DMaker 실습

---
+++ SpringFramework/실습/dmaker/src/main/java/com/fastcampus/programming/dmaker/DeveloperMakerApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.programming.dmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperMakerApplication.class, args);
+	}
+
+}
